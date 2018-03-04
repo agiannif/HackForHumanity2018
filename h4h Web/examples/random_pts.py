2087eea75231cd05106ff9473e3d87327d12c710
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-03 21:17:58 -0800

    replacing project

--- h4h Web/examples/random_pts.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-test_data_x = (np.random.uniform(37.782, 37.784, 100))
-test_data_y = (np.random.uniform(-122.445, -122.39, 100))
-for i in range(100):
-    print('{lat: ',test_data_x[i],', lng: ',test_data_y[i],'},')
