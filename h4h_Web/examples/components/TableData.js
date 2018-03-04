fb831a38babad0c8eb8cc7cd0de17d077d1c0829
Dan Barkhorn <danielbarkhorn@gmail.com>
2018-03-04 10:17:42 -0800

    table data

--- h4h_Web/examples/components/TableData.js
+++ h4h_Web/examples/components/TableData.js
@@ -3,45 +3,42 @@ import ReactDOM from 'react-dom'
 import styles from './autocomplete.module.css'
 
 var i = 0;
+var items = [{
+        "firstName": "Sam",
+        "lastName": "Bellay",
+        "numPeople": "1",
+        "emergencyLevel": "2",
+        "fire": "false",
+        "flood": "false",
+        "earthquake": "true",
+        "trapped": "false",
+        "child": "true",
+        "injury": "false",
+        "disability": "false",
+        "elderly": "false",
+        "time": "1520153541",
+        "latitude": "37.3542",
+        "longitude": "-121.9551"}
+
+    ];
 
 class TableData extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {items: [{
-            "firstName": "Sam",
-            "lastName": "Bellay",
-            "numPeople": "1",
-            "emergencyLevel": "2",
-            "fire": "false",
-            "flood": "false",
-            "earthquake": "true",
-            "trapped": "false",
-            "child": "true",
-            "injury": "false",
-            "disability": "false",
-            "elderly": "false",
-            "time": "1520153541",
-            "latitude": "37.3542",
-            "longitude": "-121.9551"
-          }
-        ]
-      };
-      console.log(i);
-    // this.flipLeft = this.flipLeft.bind(this);
-    // this.flipRight = this.flipRight.bind(this);
+
+    this.flipLeft = this.flipLeft.bind(this);
+    this.flipRight = this.flipRight.bind(this);
+    this.refreshData = this.refreshData.bind(this);
   }
 
   flipRight() {
     i= i+1;
-    console.log(i);
-    console.log(this);
-    this.render();
+    this.render()
   }
 
   flipLeft() {
     i=i-1;
-    console.log(i);
-    this.render();
+    this.render()
   }
 
   refreshData() {
@@ -50,9 +47,10 @@ class TableData extends React.Component {
       return response.json();
     })
     .then(function(json) {
-      var data = json['data'].push(this.items);
-      console.log('done2', json);
-      this.setState(items: json);
+      console.log('done2', json['data']);
+      items = json['data'];
+      console.log('hey',items);
+      this.render();
     })
     .catch(function(error) {
       return;
@@ -61,24 +59,25 @@ class TableData extends React.Component {
 
   render() {
     console.log(i);
+    console.log(items[i]);
     return (
         <div>
           <table>
-              <tr><td>Name</td><td>{this.state.items[i]['firstName']} {this.state.items[i]['lastName']}</td></tr>
-              <tr><td>Number of People</td><td>{this.state.items[i]['numPeople']}</td></tr>
-              <tr><td>Level of Emergency</td><td>{this.state.items[i]['emergencyLevel']}</td></tr>
-              <tr><td>Fire?</td><td>{this.state.items[i]['fire']}</td></tr>
-              <tr><td>Flood?</td><td>{this.state.items[i]['flood']}</td></tr>
-              <tr><td>Trapped?</td><td>{this.state.items[i]['trapped']}</td></tr>
-              <tr><td>Children?</td><td>{this.state.items[i]['child']}</td></tr>
-              <tr><td>Injury?</td><td>{this.state.items[i]['injury']}</td></tr>
-              <tr><td>Disability?</td><td>{this.state.items[i]['disability']}</td></tr>
-              <tr><td>Elderly?</td><td>{this.state.items[i]['elderly']}</td></tr>
+              <tr><td>Name</td><td>{items[i]['firstName']} {items[i]['lastName']}</td></tr>
+              <tr><td>Number of People</td><td>{items[i]['numPeople']}</td></tr>
+              <tr><td>Level of Emergency</td><td>{items[i]['emergencyLevel']}</td></tr>
+              <tr><td>Fire?</td><td>{items[i]['fire']}</td></tr>
+              <tr><td>Flood?</td><td>{items[i]['flood']}</td></tr>
+              <tr><td>Trapped?</td><td>{items[i]['trapped']}</td></tr>
+              <tr><td>Children?</td><td>{items[i]['child']}</td></tr>
+              <tr><td>Injury?</td><td>{items[i]['injury']}</td></tr>
+              <tr><td>Disability?</td><td>{items[i]['disability']}</td></tr>
+              <tr><td>Elderly?</td><td>{items[i]['elderly']}</td></tr>
         </table>
         <div className={styles.buttons}>
-          <input type="submit" value="Left" onClick={this.flipLeft}/>
+          <input type="submit" value="Previous" onClick={this.flipLeft}/>
           <input type="submit" value="Refresh" onClick={this.refreshData}/>
-          <input type="submit" value="After" onClick={this.flipRight}/>
+          <input type="submit" value="Next" onClick={this.flipRight}/>
         </div>
       </div>
     );
